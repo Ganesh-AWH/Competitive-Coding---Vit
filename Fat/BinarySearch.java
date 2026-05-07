d8e111092a0e4b108b5d4ff95d3e1ac07536d514
Ganesh <ganesh02003@gmail.com>
2026-05-07 11:46:22 +0530

    binarySearch algorithm

---
+++ Fat/BinarySearch.java
@@ -0,0 +1,24 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int []arr = new int[]{1, 5, 8, 10, 12, 40, 55, 100};
+        int index = binarySearchIterative(arr, 101);
+        System.out.println(index);
+    }
+    public static int binarySearchIterative(int []arr, int target){
+        int low = 0;
+        int high = arr.length-1;
+
+        while(low <= high){
+            int mid = (high - low)/2+low;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] > target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+}
